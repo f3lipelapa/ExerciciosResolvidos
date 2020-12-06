b6c45fe4798557c57517a35b7cb282d7d7529a7e
Felipe Lapa do Nascimento <felipe.7lapa@hotmail.com>
2020-12-06 12:15:16 -0300

    Validação de IP

---
+++ Python/Arquivos/ValidaçãoIP/validacao.py
@@ -0,0 +1,22 @@
+import socket as sk
+
+arquivo = open('ips', 'r')
+lista_IP_val = []
+lista_IP_inval = []
+
+for IP in arquivo:
+    try:
+        if sk.inet_aton(IP):
+            lista_IP_val.append(IP)
+    except sk.error:
+        lista_IP_inval.append(IP)
+
+arquivo.close()
+
+print('Endereços válidos:')
+for IP_val in lista_IP_val:
+    print(IP_val, end='')
+
+print('\nEndereços inválidos:')
+for IP_inval in lista_IP_inval:
+    print(IP_inval, end='')
