a47db45572bbc004c61665a985ab4f1beecbfc50
Felipe Lapa do Nascimento <felipe.7lapa@hotmail.com>
2020-12-06 12:19:25 -0300

    Função
    O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, de forma a agilizar a execução do programa. A conversão da espaço ocupado em disco, de bytes para megabytes deverá ser feita através de uma função separada, que será chamada pelo programa principal. O cálculo do percentual de uso também deverá ser feito através de uma função, que será chamada pelo programa principal.

---
+++ Python/Arquivos/Espaço Funcionário/Func_convert/__init__.py
@@ -0,0 +1,8 @@
+def Converter(valor):
+    megaby = valor / 1048576
+    return megaby
+
+def Converter_P(valor):
+    calculo = valor / 2581.57
+    porcentagem = calculo * 100
+    return porcentagem
